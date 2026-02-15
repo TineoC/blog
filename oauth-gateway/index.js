7271af1c10d7ca33238269ea37117ba1372854ed
Christopher Tineo <christophertineo02@gmail.com>
2026-02-15 00:57:31 -0500

    Configure Decap CMS to use custom OAuth gateway

---
+++ oauth-gateway/index.js
@@ -0,0 +1,48 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/auth") {
+      return Response.redirect(
+        `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo&allow_signup=true`,
+        302
+      );
+    }
+
+    if (url.pathname === "/callback") {
+      const code = url.searchParams.get("code");
+      const response = await fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "accept": "application/json",
+        },
+        body: JSON.stringify({
+          client_id: env.GITHUB_CLIENT_ID,
+          client_secret: env.GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      });
+      const result = await response.json();
+      
+      return new Response(`
+        <script>
+          const receiveMessage = (message) => {
+            window.opener.postMessage(
+              'authorization:github:success:' + JSON.stringify({
+                token: "${result.access_token}",
+                provider: "github"
+              }),
+              message.origin
+            );
+            window.removeEventListener("message", receiveMessage, false);
+          }
+          window.addEventListener("message", receiveMessage, false);
+          window.opener.postMessage("authorizing:github", "*");
+        </script>
+      `, { headers: { "content-type": "text/html" } });
+    }
+
+    return new Response("Not Found", { status: 404 });
+  }
+};
